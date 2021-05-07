8396febb8810c81820d50f2b0f832a1234a1f14f
soumya1999rta <79540393+soumya1999rta@users.noreply.github.com>
2021-05-07 18:15:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,6 +25,253 @@ import av
 import cv2
 import numpy as np
 import streamlit as st
+# import dependencies
+from IPython.display import display, Javascript, Image,clear_output
+from google.colab.output import eval_js
+from base64 import b64decode, b64encode
+import cv2
+import numpy as np
+import PIL
+import io
+import html
+import time
+
+# function to convert the JavaScript object into an OpenCV image
+def js_to_image(js_reply):
+  """
+  Params:
+          js_reply: JavaScript object containing image from webcam
+  Returns:
+          img: OpenCV BGR image
+  """
+  # decode base64 image
+  image_bytes = b64decode(js_reply.split(',')[1])
+  # convert bytes to numpy array
+  jpg_as_np = np.frombuffer(image_bytes, dtype=np.uint8)
+  # decode numpy array into OpenCV BGR image
+  img = cv2.imdecode(jpg_as_np, flags=1)
+
+  return img
+
+# function to convert OpenCV Rectangle bounding box image into base64 byte string to be overlayed on video stream
+def bbox_to_bytes(bbox_array):
+  """
+  Params:
+          bbox_array: Numpy array (pixels) containing rectangle to overlay on video stream.
+  Returns:
+        bytes: Base64 image byte string
+  """
+  # convert array into PIL image
+  bbox_PIL = PIL.Image.fromarray(bbox_array, 'RGBA')
+  iobuf = io.BytesIO()
+  # format bbox into png for return
+  bbox_PIL.save(iobuf, format='png')
+  # format return string
+  bbox_bytes = 'data:image/png;base64,{}'.format((str(b64encode(iobuf.getvalue()), 'utf-8')))
+
+  return bbox_bytes
+
+# initialize the Haar Cascade face detection model
+face_cascade = cv2.CascadeClassifier(cv2.samples.findFile(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'))
+
+# JavaScript to properly create our live video stream using our webcam as input
+def video_stream():
+  js = Javascript('''
+    var video;
+    var div = null;
+    var stream;
+    var captureCanvas;
+    var imgElement;
+    var labelElement;
+    
+    var pendingResolve = null;
+    var shutdown = false;
+    
+    function removeDom() {
+       stream.getVideoTracks()[0].stop();
+       video.remove();
+       div.remove();
+       video = null;
+       div = null;
+       stream = null;
+       imgElement = null;
+       captureCanvas = null;
+       labelElement = null;
+    }
+    
+    function onAnimationFrame() {
+      if (!shutdown) {
+        window.requestAnimationFrame(onAnimationFrame);
+      }
+      if (pendingResolve) {
+        var result = "";
+        if (!shutdown) {
+          captureCanvas.getContext('2d').drawImage(video, 0, 0, 640, 480);
+          result = captureCanvas.toDataURL('image/jpeg', 0.8)
+        }
+        var lp = pendingResolve;
+        pendingResolve = null;
+        lp(result);
+      }
+    }
+    
+    async function createDom() {
+      if (div !== null) {
+        return stream;
+      }
+
+      div = document.createElement('div');
+      div.style.border = '2px solid black';
+      div.style.padding = '3px';
+      div.style.width = '100%';
+      div.style.maxWidth = '600px';
+      document.body.appendChild(div);
+      
+      const modelOut = document.createElement('div');
+      modelOut.innerHTML = "<span>Status:</span>";
+      labelElement = document.createElement('span');
+      labelElement.innerText = 'No data';
+      labelElement.style.fontWeight = 'bold';
+      modelOut.appendChild(labelElement);
+      div.appendChild(modelOut);
+           
+      video = document.createElement('video');
+      video.style.display = 'block';
+      video.width = div.clientWidth - 6;
+      video.setAttribute('playsinline', '');
+      video.onclick = () => { shutdown = true; };
+      stream = await navigator.mediaDevices.getUserMedia(
+          {video: { facingMode: "environment"}});
+      div.appendChild(video);
+
+      imgElement = document.createElement('img');
+      imgElement.style.position = 'absolute';
+      imgElement.style.zIndex = 1;
+      imgElement.onclick = () => { shutdown = true; };
+      div.appendChild(imgElement);
+      
+      const instruction = document.createElement('div');
+      instruction.innerHTML = 
+          '<span style="color: red; font-weight: bold;">' +
+          'When finished, click here or on the video to stop this demo</span>';
+      div.appendChild(instruction);
+      instruction.onclick = () => { shutdown = true; };
+      
+      video.srcObject = stream;
+      await video.play();
+
+      captureCanvas = document.createElement('canvas');
+      captureCanvas.width = 640; //video.videoWidth;
+      captureCanvas.height = 480; //video.videoHeight;
+      window.requestAnimationFrame(onAnimationFrame);
+      
+      return stream;
+    }
+    async function stream_frame(label, imgData) {
+      if (shutdown) {
+        removeDom();
+        shutdown = false;
+        return '';
+      }
+
+      var preCreate = Date.now();
+      stream = await createDom();
+      
+      var preShow = Date.now();
+      if (label != "") {
+        labelElement.innerHTML = label;
+      }
+            
+      if (imgData != "") {
+        var videoRect = video.getClientRects()[0];
+        imgElement.style.top = videoRect.top + "px";
+        imgElement.style.left = videoRect.left + "px";
+        imgElement.style.width = videoRect.width + "px";
+        imgElement.style.height = videoRect.height + "px";
+        imgElement.src = imgData;
+      }
+      
+      var preCapture = Date.now();
+      var result = await new Promise(function(resolve, reject) {
+        pendingResolve = resolve;
+      });
+      shutdown = false;
+      
+      return {'create': preShow - preCreate, 
+              'show': preCapture - preShow, 
+              'capture': Date.now() - preCapture,
+              'img': result};
+    }
+    ''')
+
+  display(js)
+  
+def video_frame(label, bbox):
+  data = eval_js('stream_frame("{}", "{}")'.format(label, bbox))
+  return data
+
+def face_detect():
+  import cv2
+  from PIL import Image
+  import numpy as np
+  import os
+  from keras.models import load_model
+  from time import sleep
+  from keras.preprocessing.image import img_to_array
+  from keras.preprocessing import image
+# start streaming video from webcam
+  video_stream()
+# label for video
+  label_html = 'Capturing...'
+# initialze bounding box to empty
+  bbox = ''
+  count = 0
+  face_classifier = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml') # Face Detection
+  classifier =load_model('Final_model.h5')  #Load model
+  emotion_labels = ['Angry','Disgust','Fear','Happy','Neutral', 'Sad', 'Surprise'] 
+
+  while True:
+    js_reply = video_frame(label_html, bbox)
+    if not js_reply:
+      break
+
+    # convert JS response to OpenCV Image
+    img = js_to_image(js_reply["img"])
+
+    # create transparent overlay for bounding box
+    bbox_array = np.zeros([480,640,4], dtype=np.uint8)
+
+    # grayscale image for face detection
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # get face region coordinates
+    faces = face_cascade.detectMultiScale(gray)
+    # get face bounding box for overlay
+    for (x,y,w,h) in faces:
+      bbox_array = cv2.rectangle(bbox_array,(x,y),(x+w,y+h),(0,255,0),2)
+      roi_gray = gray[y:y+h,x:x+w]
+      roi_gray = cv2.resize(roi_gray,(48,48),interpolation=cv2.INTER_AREA)  ##Face Cropping for prediction
+      if np.sum([roi_gray])!=0:
+        roi = roi_gray.astype('float')/255.0
+        roi = img_to_array(roi)
+        roi = np.expand_dims(roi,axis=0) ## reshaping the cropped face image for prediction
+        prediction = classifier.predict(roi)[0]   #Prediction
+        label=emotion_labels[prediction.argmax()]
+        label_position = (x,y)
+        cv2.putText(bbox_array,label,label_position,cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)   # Text Adding
+      else:
+        cv2.putText(bbox_array,'No Faces',(30,80),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
+
+    
+
+
+
+    bbox_array[:,:,3] = (bbox_array.max(axis = 2) > 0 ).astype(int) * 255
+    # convert overlay of bbox into bytes
+    bbox_bytes = bbox_to_bytes(bbox_array)
+    # update bbox so next frame gets new overlay
+    bbox = bbox_bytes
+
 
 
 from streamlit_webrtc import (
@@ -60,48 +307,7 @@ except Exception:
     st.write("Error loading cascade classifiers")
 
 
-def detect():
-    '''
-    Function to detect emotions of faces passed to this function.
-    '''
-    cap = cv2.VideoCapture(0)
-    #print("WebCam is Working.Proceed")
-    if not cap.isOpened():
-        cap = cv2.VideoCapture(1)
-    #print("WebCam is Working.Proceed")
-    if not cap.isOpened():
-    #print("WebCam isnot Working.")
-        raise IOError("Cannot use webcam")
-    while True:
-        _, frame = cap.read()
-        labels = []
-        gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-        faces = face_classifier.detectMultiScale(gray)
 
-        for (x,y,w,h) in faces:
-            cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,255),2)
-            roi_gray = gray[y:y+h,x:x+w]
-            roi_gray = cv2.resize(roi_gray,(48,48),interpolation=cv2.INTER_AREA)  ##Face Cropping for prediction
-
-
-
-            if np.sum([roi_gray])!=0:
-                roi = roi_gray.astype('float')/255.0
-                roi = img_to_array(roi)
-                roi = np.expand_dims(roi,axis=0) ## reshaping the cropped face image for prediction
-
-                prediction = classifier.predict(roi)[0]   #Prediction
-                label=emotion_labels[prediction.argmax()]
-                label_position = (x,y)
-                cv2.putText(frame,label,label_position,cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)   # Text Adding
-            else:
-                cv2.putText(frame,'No Faces',(30,80),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
-        cv2.imshow('Emotion Detector',frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-    cap.release()
-    cv2.destroyAllWindows()
     
     
 
@@ -224,7 +430,7 @@ def main():
                   5. Still webcam window didnot open,  go to Check Camera from the sidebar.''')
         
         if st.button("Proceed"):
-            detect()
+            face_detect()
     
     
     elif choice == "Check Camera":
